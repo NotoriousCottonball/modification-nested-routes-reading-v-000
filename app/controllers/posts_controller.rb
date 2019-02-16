db6c7922d1eeafbdca5e470e24eaa83965842be1
George  Pianka <gpianka1@gmail.com>
2019-02-16 16:37:06 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -37,10 +37,8 @@ class PostsController < ApplicationController
       if author = Author.find_by(id: params[:author_id])
         @post = author.posts.find_by(id: params[:id])
         redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
-        
       else
         redirect_to authors_path, alert: "Author not found."
-        
       end
     else
       @post = Post.find(params[:id])
